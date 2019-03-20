6045a126608580324b32c24c0e5dce9566a4a387
Kantoki <victorcdauphine@gmail.com>
2019-03-20 19:21:36 +0100

    -- Change Mapping

--- src/main/java/com/yukart/controllers/ConnexionForm.java
+++ src/main/java/com/yukart/controllers/ConnexionForm.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ConnexionForm {
-	
-	@GetMapping("/")
+
+	@GetMapping("/login")
 	public String connexion() {
 		return "views/connexion";
-		
+
 	}
 
 }
